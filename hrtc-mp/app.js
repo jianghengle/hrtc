9ce9cc97f7d496aae35f8462cf5883496218c209
jianghengle <jianghengle@gmail.com>
2024-01-04 22:28:28 -0800

    init mp and sam

---
+++ hrtc-mp/app.js
@@ -0,0 +1,70 @@
+//app.js
+App({
+  onLaunch: function() {
+    // 展示本地存储能力
+    //var logs = wx.getStorageSync('logs') || []
+    //logs.unshift(Date.now())
+    //wx.setStorageSync('logs', logs)
+    /*
+    wx.request({
+      url: 'http://127.0.0.1:3000/org/check-org-id',
+      method: 'POST',
+      data: {
+        orgId: '31cc',
+      },
+      success (res) {
+        console.log('success', res.data)
+      },
+      fail (err) {
+        console.log('fail', err)
+      },
+    })
+
+    // 登录
+    wx.login({
+      success: res => {
+        console.log('login', res)
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      }
+    })
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        console.log('getSetting', res)
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              console.log('getUserInfo', res)
+              // 可以将 res 发送给后台解码出 unionId
+              this.globalData.userInfo = res.userInfo
+
+              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+              // 所以此处加入 callback 以防止这种情况
+              if (this.userInfoReadyCallback) {
+                this.userInfoReadyCallback(res)
+              }
+            }
+          })
+        }
+      }
+    })
+    */
+    // 获取系统状态栏信息
+    wx.getSystemInfo({
+      success: e => {
+        this.globalData.StatusBar = e.statusBarHeight;
+        let capsule = wx.getMenuButtonBoundingClientRect();
+        if (capsule) {
+         	this.globalData.Custom = capsule;
+        	this.globalData.CustomBar = capsule.bottom + capsule.top - e.statusBarHeight;
+        } else {
+        	this.globalData.CustomBar = e.statusBarHeight + 50;
+        }
+      }
+    })
+  },
+  globalData: {
+    userInfo: null
+  }
+})
